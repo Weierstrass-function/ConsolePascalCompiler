78bdfba45ddf87e8911a84aaaef46ae28c1e93ae
Faust < >
2025-05-25 15:27:29 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Компилятор
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
